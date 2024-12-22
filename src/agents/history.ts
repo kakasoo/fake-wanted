b8fb631ab8e4cce1945b314b09fc9b3ad03acd1a
kakasoo <kscodebase@gmail.com>
2024-12-22 17:21:43 +0900

    feat: system과 history를 넣는 프롬프트들을 분리

---
+++ src/agents/history.ts
@@ -0,0 +1,36 @@
+import { ChatCompletionMessageParam } from "openai/resources";
+
+import { RoomProvider } from "../providers/room/RoomProvider";
+
+export namespace History {
+  export function prompt(room: Awaited<ReturnType<ReturnType<typeof RoomProvider.at>>>) {
+    if (room.chattings.length === 0) {
+      return [];
+    }
+
+    return room.chattings.map((history): ChatCompletionMessageParam => {
+      const role = history.speaker as "user" | "assistant";
+      if (role === "user") {
+        return {
+          role: "user",
+          content: JSON.stringify({
+            room_id: history.room_id,
+            user_id: history.user_id,
+            speaker: history.speaker,
+            created_at: history.created_at,
+          }),
+        };
+      } else {
+        return {
+          role: "assistant",
+          content: JSON.stringify({
+            room_id: history.room_id,
+            user_id: history.user_id,
+            speaker: history.speaker,
+            created_at: history.created_at,
+          }),
+        };
+      }
+    });
+  }
+}
---
+++ src/agents/system.ts
@@ -0,0 +1,46 @@
+import { readFileSync } from "fs";
+import { ChatCompletionSystemMessageParam } from "openai/resources";
+import { join } from "path";
+
+export namespace System {
+  export function getSchemaInfo(): string {
+    const filepath = join(__dirname, "../../packages/api/openai-positional.json");
+    const schema = readFileSync(filepath, { encoding: "utf-8" });
+    return schema;
+  }
+
+  export function prompt(): ChatCompletionSystemMessageParam {
+    return {
+      role: "system",
+      content: [
+        "This schema information is information about external APIs that you can call.",
+        "You have to find function that the user requires here.",
+        "If you find a function, you must define the input parameters for executing it.",
+        "[Caution] From here down is the schema.",
+        `${System.getSchemaInfo()}`,
+        "[Caution] From here, the top is the schema.",
+        "",
+        "All chats with users are talked through the markdown viewer, so you always have to say markdown.",
+        "",
+        "Your response format is always one of them:",
+        '1. `{ "type": "chat", "message": string }`',
+        '2. `{ "type": "selectFunction", "functions": Array<{ method: "get" | "post" | "delete" | "put" | "patch", pathname: string }>, "message": string }`',
+        '3. `{ "type": "fillArgument", "method": "get" | "post" | "delete" | "put" | "patch", pathname: string, "parameters": JSON, "message": string }`',
+        "",
+        "'fillArgument' is JSON, and each JSON object should be mapped under the fki names 'query', 'body', and 'param'.",
+        'for example, { "parameters": { "body": { "query": "EXAMPLE_TEXT" } } }',
+        "[Caution] All message types must adhere to the JSON format.",
+        "'chat' is just sending message as markdown format.",
+        "'selectFunction' is selecting method and method property must be method name like as get, post, delete, put, patch, and pathname, for example, 'monitors/health'",
+        "'fillArguments' is to listen to the user and directly fill the factor values to fill the function.",
+        "If it is not enough to fill yet, you should ask the user to secure the factor, and this question corresponds to the 'chat' type.",
+        "'fillArguments' must not appear before 'selectFunction'.",
+        "'fillArguments' must be an extension of the type of 'selectFunction' because it fills the parameters.",
+        "",
+        "You have to finally get to the stage of calling the function, so you have to talk to the user and finally go to 'fillArguments' three times.",
+        "If you explain this response to the user, the user will decide whether to call the function or not.",
+        "",
+      ].join("\n"),
+    };
+  }
+}
