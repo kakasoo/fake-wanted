c509c8e6afabe0cf23b7e28508ec3b704a17368d
kakasoo <kscodebase@gmail.com>
2024-12-27 00:31:44 +0900

    test: fillArgument 객체를 이용한 함수 호출 성공 여부 검사

--- test/features/api/wanted/test_api_wanted.ts
+++ test/features/api/wanted/test_api_wanted.ts
@@ -2,10 +2,29 @@ import typia from "typia";
 
 import api from "@kakasoo/fake-wanted-api";
 
+import { RunFunctionAgent } from "../../../../src/agent/runFunction/runFunction";
+
 export async function test_api_wanted(connection: api.IConnection): Promise<void> {
   const res = await api.functional.wanted.search(connection, {
     query: "뤼튼",
   });
 
   typia.assert(res);
+}
+
+export async function test_api_wanted_with_fill_argument() {
+  const fillArgument = {
+    type: "fillArgument",
+    method: "get",
+    pathname: "/wanted",
+    parameters: {
+      query: { query: "원티드" },
+      body: {},
+      param: {},
+    },
+    message: "",
+  } as const;
+
+  const response = await RunFunctionAgent.functionCall(fillArgument);
+  console.log(response);
 }
