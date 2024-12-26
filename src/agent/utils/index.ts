e653ce857e05569ddce4371979cc81480bbaa4cd
kakasoo <kscodebase@gmail.com>
2024-12-27 00:35:06 +0900

    feat: util 함수를 추가된 Agent에 맞게 확장

--- src/agent/utils/index.ts
+++ src/agent/utils/index.ts
@@ -2,11 +2,27 @@ import { ChatCompletion } from "openai/resources";
 import typia from "typia";
 
 import { MessageType as AnswerMessageType } from "../answer/IMessageType";
+import { MessageType as FillArgumentMessageType } from "../fillArugment/IMessageType";
 import { MessageType as JudgementMessageType } from "../judgement/IMessageType";
 import { MessageType as SelectFunctionMessageType } from "../selectFunction/IMessageType";
 
 export namespace AgentUtil {
   export namespace Content {
+    export function runFunction(input: ChatCompletion) {
+      const content = input.choices.at(0)?.message.content ?? null;
+      return content;
+    }
+
+    export function fillArgument(input: ChatCompletion): FillArgumentMessageType {
+      const content = input.choices.at(0)?.message.content ?? null;
+      const response = content === null ? null : typia.json.isParse<FillArgumentMessageType | null>(content);
+      if (response === null) {
+        throw new Error(`invalid fillArgument message type: ${content}`);
+      }
+
+      return response;
+    }
+
     export function selectFunction(input: ChatCompletion): SelectFunctionMessageType {
       const content = input.choices.at(0)?.message.content ?? null;
       const response = content === null ? null : typia.json.isParse<SelectFunctionMessageType | null>(content);
@@ -43,16 +59,22 @@ export namespace AgentUtil {
     }
   }
 
+  export function getContent(role: "runFunction"): typeof AgentUtil.Content.runFunction;
+  export function getContent(role: "fillArgument"): typeof AgentUtil.Content.fillArgument;
   export function getContent(role: "selectFunction"): typeof AgentUtil.Content.selectFunction;
   export function getContent(role: "answer"): typeof AgentUtil.Content.answer;
   export function getContent(role: "judgement"): typeof AgentUtil.Content.judgement;
-  export function getContent(role: "answer" | "judgement" | "selectFunction") {
+  export function getContent(role: "answer" | "judgement" | "selectFunction" | "fillArgument" | "runFunction") {
     if (role === "answer") {
       return AgentUtil.Content.answer;
     } else if (role === "judgement") {
       return AgentUtil.Content.judgement;
     } else if (role === "selectFunction") {
       return AgentUtil.Content.selectFunction;
+    } else if (role === "fillArgument") {
+      return AgentUtil.Content.fillArgument;
+    } else if (role === "runFunction") {
+      return AgentUtil.Content.runFunction;
     } else {
       throw new Error(`invalid role type: ${role}`);
     }
---
+++ src/agent/utils/retry.ts
@@ -0,0 +1,6 @@
+// // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+// export async function retry(fn: Function, count: number) {
+//   while (count--) {
+//     const response = await fn();
+//   }
+// }
