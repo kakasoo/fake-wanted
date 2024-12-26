cdbc0d84b3c2b4b4531ed954a974d97d9d922eb6
kakasoo <kscodebase@gmail.com>
2024-12-27 00:32:35 +0900

    feat: selectFunction에 스키마 정보를 넣고 설명까지 이어지도록 프롬프트 수정

--- src/agent/selectFunction/selectFunction.ts
+++ src/agent/selectFunction/selectFunction.ts
@@ -11,31 +11,26 @@ import { AgentUtil } from "../utils";
 import { System } from "./system";
 
 export namespace SelectFunctionAgent {
-  export const chat =
-    (room: Awaited<ReturnType<ReturnType<typeof RoomProvider.at>>>) => async (input: { message: string }) => {
-      const histories = Scribe.prompt(room, ["answer"]);
+  export const chat = (room: Awaited<ReturnType<ReturnType<typeof RoomProvider.at>>>) => async () => {
+    const histories = Scribe.prompt(room, ["selectFunction"]);
 
-      // answer의 시스템 프롬프트가 1번 이상 들어가는 것을 방지하기 위해 탐색
-      const systemPrompt = histories.find((el) => {
-        return el.role === "system" && (JSON.parse(el.content).role as IAgent.Role) === "selectFunction";
-      });
+    // answer의 시스템 프롬프트가 1번 이상 들어가는 것을 방지하기 위해 탐색
+    const systemPrompt = histories.find((el) => {
+      return el.role === "system" && (JSON.parse(el.content).role as IAgent.Role) === "selectFunction";
+    });
 
-      const chatCompletion = await new OpenAI({
-        apiKey: process.env.OPEN_AI_KEY,
-      }).chat.completions.create({
-        model: "gpt-4o-mini",
-        messages: [
-          ...(systemPrompt ? [] : [System.prompt()]), // Answer 용 시스템 프롬프트가 주입 안된 경우에 주입한다.
-          ...histories,
-          {
-            role: "user",
-            content: input.message,
-          },
-        ],
-      });
+    const chatCompletion = await new OpenAI({
+      apiKey: process.env.OPEN_AI_KEY,
+    }).chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        ...histories,
+        ...(systemPrompt ? [] : [System.prompt()]), // Answer 용 시스템 프롬프트가 주입 안된 경우에 주입한다.
+      ],
+    });
 
-      return AgentUtil.getContent("selectFunction")(chatCompletion);
-    };
+    return AgentUtil.getContent("selectFunction")(chatCompletion);
+  };
 
   export const answer =
     (user: IEntity) =>
@@ -44,12 +39,12 @@ export namespace SelectFunctionAgent {
       const room = await RoomProvider.at(user)({ id: input.roomId });
 
       // 2. 유저의 발화 내용을 기반으로 응답을 호출한다.
-      const answer = await SelectFunctionAgent.chat(room)(input);
+      const answer = await SelectFunctionAgent.chat(room)();
       const response = await ChatProvider.create({
         userId: user.id,
         roomId: input.roomId,
-        speaker: "assistent",
-        message: answer.message,
+        speaker: "assistant",
+        message: JSON.stringify(answer),
         role: null,
       });
 
