6a765d19282b85b9eb9a0b0e69c5da403b3527ff
kakasoo <kscodebase@gmail.com>
2024-12-29 17:00:25 +0900

    feat: 메시지 타입 별로 다음 Transition을 담은 functions 네임스페이스 정의

--- src/agent/judgement/IMessageType.ts
+++ src/agent/judgement/IMessageType.ts
@@ -3,7 +3,7 @@ import typia from "typia";
 
 export namespace MessageType {
   export interface Tool {
-    call(input: { type: "chat" | "selectFunction" | "fillArgument" | "runFunction" }): never;
+    call(input: MessageType): never;
   }
 
   export const functions = typia.llm.application<Tool, "chatgpt">().functions.map((func): ChatCompletionTool => {
@@ -16,8 +16,116 @@ export namespace MessageType {
       },
     };
   });
+
+  export type Transition<T extends MessageType["type"]> = T extends "chat"
+    ? "chat" | "selectFunction" | "fillArgument" | "runFunction"
+    : T extends "selectFunction"
+      ? "chat" | "fillArgument" | "runFunction"
+      : T extends "fillArgument"
+        ? "chat" | "runFunction"
+        : "chat";
+
+  export namespace Functions {
+    export const chat = typia.llm
+      .application<
+        {
+          /**
+           * Function that pulls out the type that can be called the next time
+           *
+           * @title call
+           */
+          call(input: { type: Transition<"chat"> }): never;
+        },
+        "chatgpt"
+      >()
+      .functions.map((func): ChatCompletionTool => {
+        return {
+          type: "function",
+          function: {
+            name: func.name,
+            description: func.description,
+            parameters: func.parameters as Record<string, any>,
+          },
+        };
+      });
+
+    export const selectFunction = typia.llm
+      .application<
+        {
+          /**
+           * Function that pulls out the type that can be called the next time
+           *
+           * @title call
+           */
+          call(input: { type: Transition<"selectFunction"> }): never;
+        },
+        "chatgpt"
+      >()
+      .functions.map((func): ChatCompletionTool => {
+        return {
+          type: "function",
+          function: {
+            name: func.name,
+            description: func.description,
+            parameters: func.parameters as Record<string, any>,
+          },
+        };
+      });
+
+    export const fillArgument = typia.llm
+      .application<
+        {
+          /**
+           * Function that pulls out the type that can be called the next time
+           *
+           * @title call
+           */
+          call(input: { type: Transition<"fillArgument"> }): never;
+        },
+        "chatgpt"
+      >()
+      .functions.map((func): ChatCompletionTool => {
+        return {
+          type: "function",
+          function: {
+            name: func.name,
+            description: func.description,
+            parameters: func.parameters as Record<string, any>,
+          },
+        };
+      });
+
+    export const runFunction = typia.llm
+      .application<
+        {
+          /**
+           * Function that pulls out the type that can be called the next time
+           *
+           * @title call
+           */
+          call(input: { type: Transition<"runFunction"> }): never;
+        },
+        "chatgpt"
+      >()
+      .functions.map((func): ChatCompletionTool => {
+        return {
+          type: "function",
+          function: {
+            name: func.name,
+            description: func.description,
+            parameters: func.parameters as Record<string, any>,
+          },
+        };
+      });
+
+    export const called = MessageType.Functions.runFunction;
+  }
 }
 
-export type MessageType = {
+export interface MessageType {
+  // NEVER CHANGE TITLE OF THIS JSDOC.
+  /**
+   * @title type
+   */
   type: "chat" | "selectFunction" | "fillArgument" | "runFunction";
-};
+}
