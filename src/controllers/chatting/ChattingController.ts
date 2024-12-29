99dae81b42e308ff9dacd29966d92d9b63ee0fce
kakasoo <kscodebase@gmail.com>
2024-12-29 20:13:36 +0900

    fix: 채팅은 API 목록에서 제외

--- src/controllers/chatting/ChattingController.ts
+++ src/controllers/chatting/ChattingController.ts
@@ -1,4 +1,4 @@
-import core, { TypedBody } from "@nestia/core";
+import core, { HumanRoute, TypedBody } from "@nestia/core";
 import { Controller } from "@nestjs/common";
 
 import { IChatting } from "@kakasoo/fake-wanted-api/lib/structures/chatting/IChatting";
@@ -9,6 +9,7 @@ import { Actor } from "../../decorators/Actor";
 
 @Controller("chatting")
 export class ChattingController {
+  @HumanRoute()
   @core.TypedRoute.Post()
   async chat(@Actor() user: IEntity, @TypedBody() input: IChatting.IChatInput): Promise<IChatting.IResponse[] | null> {
     return JudgementAgent.answer(user)(input);
