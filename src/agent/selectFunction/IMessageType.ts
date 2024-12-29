c6e0c6574b95b94d0adefd1fe7030c8f632d40ee
kakasoo <kscodebase@gmail.com>
2024-12-29 15:45:44 +0900

    feat: selectFunction의 메세지 타입에 대한 타입 주석 및 functions 정의

--- src/agent/selectFunction/IMessageType.ts
+++ src/agent/selectFunction/IMessageType.ts
@@ -1,12 +1,59 @@
-export namespace MessageType {}
+import { ChatCompletionTool } from "openai/resources";
+import typia from "typia";
 
-export type MessageType = {
+export namespace MessageType {
+  export interface Tool {
+    call(input: MessageType): never;
+  }
+
+  export const functions = typia.llm.application<Tool, "chatgpt">().functions.map((func): ChatCompletionTool => {
+    return {
+      type: "function",
+      function: {
+        name: func.name,
+        description: func.description,
+        parameters: func.parameters as Record<string, any>,
+      },
+    };
+  });
+}
+
+export interface MessageType {
   type: "selectFunction";
   functions: Array<{
+    /**
+     * @title Function Method
+     */
     method: "get" | "post" | "delete" | "put" | "patch";
+
+    /**
+     * @title Function pathname without host
+     */
     pathname: string;
-    parameters: any;
+
+    /**
+     * @title Function's Parameters
+     */
+    parameters: {
+      /**
+       * @title Query Parameter Object
+       */
+      query?: object;
+
+      /**
+       * @title Request Body Object
+       */
+      body?: object;
+
+      /**
+       * @title Request Param Object
+       */
+      param?: object;
+    };
   }>;
-  message: string;
-  isSafeMethod: boolean;
-};
+
+  /**
+   * @title A secure method with no problem calling at the same time
+   */
+  isSafeMethod?: boolean;
+}
