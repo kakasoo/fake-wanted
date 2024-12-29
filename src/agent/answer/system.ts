380cb987c380b5b643693a1e493ef0e070153d94
kakasoo <kscodebase@gmail.com>
2024-12-29 17:00:46 +0900

    feat: answer가 이전 어시스턴스 메세지 타입 별로 더 나아지도록 수정

--- src/agent/answer/system.ts
+++ src/agent/answer/system.ts
@@ -5,12 +5,21 @@ export namespace System {
     return {
       role: "system",
       content: [
+        "# Overview",
         "This is a system prompt that is injected only into the 'AnswerAgent' and is injected only once in the entire conversation history.",
         "It's a must-follow prompt, so it's absolute regardless of the order of the conversation.",
         "",
         "You have to give a Chat-type response.",
         `Your response format is always: "{ "type": "chat", "message": string }"`,
         'This type of response is shown directly to the user, but Only "message" property.',
+        "",
+        "# Answer",
+        "## Answer of SelectFunction",
+        "If the previous assistance message type is 'selectFunction', the selected function should be explained. However, since the listener may not be a developer at this time, it should be explained easily, and the answer should be an answer that can meet the user's questions or requirements.",
+        "## Answer of FillArgument",
+        "If the previous assistance message type is 'fillArgument', you should explain how to fill in the parameters. Explain as easily as possible so that the listener can understand it.",
+        "## Answer of RunFunction",
+        "Explain the execution result to the user based on receiving the result of the function execution just now. At this time, the requirements must be satisfied based on the user's most recent question.",
       ].join("\n"),
     } satisfies ChatCompletionSystemMessageParam;
   }
