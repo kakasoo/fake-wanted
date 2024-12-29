66fe94731dd742edf269e4d3e54cfa58740a0163
kakasoo <kscodebase@gmail.com>
2024-12-29 20:13:19 +0900

    feat: wanted type 중 포지션 조회를 위한 타입을 정의

--- src/api/structures/watned/IWanted.ts
+++ src/api/structures/watned/IWanted.ts
@@ -1,11 +1,18 @@
 import { tags } from "typia";
 
+import { IPage } from "../common/IPage";
+
 export namespace IWanted {
   export interface ISearchOutput {
-    user_recent_search_keyword: any;
-    keywords: string[];
-    company_tags: string[];
+    /**
+     * The list of companies most similar to the search term is viewed.
+     *
+     * @title List of companies searched
+     */
     companies: null | Array<{
+      /**
+       * @title Company ID
+       */
       id: number;
       logo_img: null | {
         origin: string & tags.Format<"uri">;
@@ -16,13 +23,17 @@ export namespace IWanted {
         thumb: string & tags.Format<"uri">;
         video: (string & tags.Format<"uri">) | null;
       };
+
+      /**
+       * @title Company Name
+       */
       name: string;
-      application_response_stats: any;
-      kreditjob_id: any;
+
+      /**
+       * @title Tags of Company
+       */
       company_tags: Array<{ tag_type_id: any; title: string; kind_title: `${number}` }>;
-      attraction_tags: null | Array<number>;
     }>;
-    category_tags: string[];
   }
 
   export interface ISearchInput {
@@ -32,6 +43,55 @@ export namespace IWanted {
      * is a Korean site.
      *
      * @title Query to search for companies
+     */
+    query: string;
+  }
+
+  export interface Position {
+    /**
+     * @title Position ID
+     */
+    id: number;
+
+    /**
+     * @title Reward
+     */
+    reward_total: string;
+
+    /**
+     * @title Company Infomation
+     */
+    company: {
+      /**
+       * @title Company ID
+       */
+      id: number;
+
+      /**
+       * @title Company Name
+       */
+      name: string;
+    };
+    title_img: null | {
+      origin: string & tags.Format<"uri">;
+      thumb: string & tags.Format<"uri">;
+      video: (string & tags.Format<"uri">) | null;
+    };
+
+    /**
+     * Position property is title of this JD. i.e. "Back-end Developer(5년이상)"
+     * @title Position
+     */
+    position: string;
+  }
+
+  export interface IGetPositionInput extends IPage.IRequest {
+    /**
+     * It is recommended to search this parameter in
+     * Korean if possible. Don't forget that 'Wanted'
+     * is a Korean site.
+     *
+     * @title Query to search for position, job description
      */
     query: string;
   }
